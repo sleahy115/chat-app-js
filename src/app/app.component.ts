f10f2a8f94d8ca63d587255d0fe500655dbb6827
sarah leahy <sarah leahy>
2017-04-08 14:07:37 -0600

    add logout function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
 import { Component } from '@angular/core';
+import { AF } from "./providers/af";
+import { Router } from "@angular/router";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [AF]
 })
 export class AppComponent {
   title = 'app works!';
+  public isLoggedIn:boolean;
+  constructor(private router:Router, public afService:AF) {
+    this.afService.af.auth.subscribe(
+      (auth)=> {
+        if(auth === null) {
+          console.log(auth);
+          this.router.navigate(['login']);
+          this.isLoggedIn = false;
+        } else {
+          console.log(auth)
+          this.isLoggedIn = true;
+          this.router.navigate(['']);
+        }
+      }
+    );
+  }
+  logout(){
+    this.afService.logout();
+  }
 }
